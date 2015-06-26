bf9356a38214f1d0536011d4f928eb05a82e377c
Alexandre-io <alexandre@gotoserv.com>
2015-06-26 11:09:20 +0200

    upgrade(core) upgrade dependencies - part 1

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
     frameworks: ['jasmine'],
 
     files : [
-        'bower_components/jquery/jquery.js',
+        'bower_components/jquery/dist/jquery.js',
         'bower_components/angular/angular.js',
         'bower_components/angular-cookie/angular-cookie.js',
         'bower_components/angular-mocks/angular-mocks.js',
